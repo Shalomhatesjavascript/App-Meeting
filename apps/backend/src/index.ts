feb51b448dcf47a7762f4a2d50e1146a35d49213
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-18 18:09:40 +0100

    chore(backend, deps): add drizzle orm and kit

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,10 @@
 import { Elysia } from 'elysia'
+import { CloudflareAdapter } from 'elysia/adapter/cloudflare-worker'
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000)
+const app = new Elysia({ adapter: CloudflareAdapter }).get('/', () => 'Hello Elysia').compile()
+
+export type ElysiaApp = typeof app
+
+export default app
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
